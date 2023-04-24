7f1949d145d2eb721ea3b79b1746722f377136cb
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2023-04-24 18:03:28 +0530

    ready to deploy

---
+++ src/assets/index.js
@@ -0,0 +1,66 @@
+import logo from "./logo.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import backend from "./backend.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import flutter from "./tech/flutter.png";
+import java from "./tech/java.png";
+import sql from "./tech/sql.png";
+import spring from "./tech/spring.png";
+import angular from "./tech/angular.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import fishifox from "./company/fishifox.png";
+import elzian from "./company/elzian.png";
+
+import CardDetails from "./CardDetails.png";
+import HelpingHands from "./HelpingHands.png";
+import ProjectSupreme from "./ProjectSupreme.jpg";
+import AdminDashboard from "./AdminDashboard.png";
+import Charity from "./Charity.png";
+import Boost from "./Boost.jpg";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  flutter,
+  java,
+  sql,
+  spring,
+  angular,
+  tailwind,
+  threejs,
+  fishifox,
+  elzian,
+  HelpingHands,
+  CardDetails,
+  ProjectSupreme,
+  AdminDashboard,
+  Charity,
+  Boost,
+};
